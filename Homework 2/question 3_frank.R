ec7564608f6273a81dd039712b6678380e31a4cb
Frank-FYC <Frank.FYC@gmail.com>
2019-03-15 02:52:18 -0500

    done with question 3

--- Homework 2/question 3_frank.R
+++ Homework 2/question 3_frank.R
@@ -5,18 +5,18 @@ library(foreach)
 library(nnet)
 require(scales)
 
-df=read.csv('./online_news.csv')
+df = read.csv('./online_news.csv')
 
 df = subset(df, select = -c(url) )
 
-df=df[sample(nrow(df), 100),]
+
 
 rmse = function(y, yhat) {
   sqrt( mean( (y - yhat)^2 ) )
 }
 
-rmse_model = do(100)*{
-
+rmse_model = do(10)*{
+  df = df[sample(nrow(df), 1000),]
   n = nrow(df)
   n_train = round(0.8*n) #take x times n, then round to nearest
   n_test = n - n_train #number of n test is n minus n_train
@@ -34,36 +34,36 @@ rmse_model = do(100)*{
               data=df_train,
               maxit = maxit)
   lm2 <<- glm(shares ~ 
-                weekday_is_friday 
-              + num_videos 
-              + data_channel_is_lifestyle 
-              + global_rate_negative_words,
+                n_tokens_content 
+              + num_imgs 
+              + data_channel_is_world 
+              + max_negative_polarity,
               data=df_train,
               maxit = maxit)
   lm3 <<- glm(shares ~ 
               . 
-              - weekday_is_friday 
-              - num_videos 
-              - data_channel_is_lifestyle
-              - global_rate_negative_words,
+              - n_tokens_content 
+              - num_imgs 
+              - data_channel_is_world
+              - max_negative_polarity,
               data=df_train,
               maxit = maxit)
   lm4 <<- glm(shares ~ (.)^2,
               data=df_train,
               maxit = maxit)
   lm5 <<- glm(shares ~ 
-                (weekday_is_friday 
-              + num_videos 
-              + data_channel_is_lifestyle 
-              + global_rate_negative_words)^2,
+                (n_tokens_content 
+              + num_imgs 
+              + data_channel_is_world 
+              + max_negative_polarity)^2,
               data=df_train,
               maxit = maxit)
   lm6 <<- glm(shares ~ 
                 (. 
-              - weekday_is_friday 
-              - num_videos 
-              - data_channel_is_lifestyle
-              - global_rate_negative_words)^2,
+              - n_tokens_content 
+              - num_imgs 
+              - data_channel_is_world
+              - max_negative_polarity)^2,
               data=df_train,
               maxit = maxit)
   
@@ -86,8 +86,46 @@ rmse_model = do(100)*{
 rmse_model
 colMeans(rmse_model)
 
-# in-sample accuracy?
-yhat_train = ifelse(predict(lm2) >= 1400, 1, 0)
-y_train = ifelse(df_train$shares >= 1400, 1, 0)
-table(y=y_train, yhat=yhat_train)
+# in-sample and out of sample accuracy?
+n = nrow(df)
+n_train = round(0.8*n) #take x times n, then round to nearest
+n_test = n - n_train #number of n test is n minus n_train
 
+train_ind = sort(sample.int(n, n_train, replace=FALSE)) # randomly sample from n, n_train times, without replacement, then sort
+df_train = df[train_ind,]
+df_test = df[-train_ind,]
+
+sample.accuracy = function(x){
+  yhat_train = ifelse(predict(x) >= 1400, 1, 0)
+  y_train = ifelse(df_train$shares >= 1400, 1, 0)
+  in.sample_table <<- table(y=y_train, yhat=yhat_train)
+  
+  probhat_test = predict(x, newdata=df_test)
+  yhat_test = ifelse(probhat_test >= 1400, 1, 0)
+  y_test = ifelse(df_test$shares >= 1400, 1, 0)
+  out.sample_table <<- table(y=y_test, yhat=yhat_test)
+}
+
+sample.accuracy(lm1)
+in.sample_table
+out.sample_table
+
+sample.accuracy(lm2)
+in.sample_table
+out.sample_table
+
+sample.accuracy(lm3)
+in.sample_table
+out.sample_table
+
+sample.accuracy(lm4)
+in.sample_table
+out.sample_table
+
+sample.accuracy(lm5)
+in.sample_table
+out.sample_table
+
+sample.accuracy(lm6)
+in.sample_table
+out.sample_table
