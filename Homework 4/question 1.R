b7cd807961ba7fd83fe09d79493c6c2580904885
Frank-FYC <Frank.FYC@gmail.com>
2019-04-23 23:11:18 -0500

    updates

--- Homework 4/question 1.R
+++ Homework 4/question 1.R
@@ -1,34 +1,12 @@
+library(ISLR)
+library(tidyverse)
+
 wine <- read.csv("wine.csv", header = TRUE)
 
-#PCA Analysis####
-
-x_wine = as.matrix(wine[,1:11])
-y_wine_quality= wine[,12]
-y_wine_color= wine[,13]
-
-# Let's try dimensionality reduction via PCA
-pc_wine = prcomp(x_wine, scale=TRUE)
-
-# pc_wine$x has the summary variables
-# Regress on the first K
-K = 3
-scores = pc_wine$x[,1:K]
-pcr1 = lm(y_wine_quality ~ scores)
-
-summary(pcr1)
-
-# Show the model fit
-plot(fitted(pcr1), y_wine_quality)
-
-# we can find best k, through train test splits, data validation. Auto-encoder = PCA. Dimensionality reduction. 
-
-# Visualize the first few principal components:
-# these are the coefficients in the linear combination for each summary
-plot(seq(1,11,by=1), pc_wine$rotation[,1])
-plot(seq(1,11,by=1), pc_wine$rotation[,2])
-plot(seq(1,11,by=1), pc_wine$rotation[,3])
-
-
+# PCA analysis####
+pr_wine = prcomp(wine, scale=TRUE)
+ 
+ 
 #Cluster Analysis####
 library(ggplot2)
 library(LICORS)  # for kmeans++
