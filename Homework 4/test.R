0d13d206a10aabf019ac96e9280c9f32be17acee
Frank-FYC <Frank.FYC@gmail.com>
2019-04-25 16:10:06 -0500

    Question 1 Completed
    Markdown works.

--- Homework 4/test.R
+++ Homework 4/test.R
@@ -1,14 +1,32 @@
-# fancy plot matrix with stuff, see http://www.sthda.com/english/wiki/scatter-plot-matrices-r-base-graphs
-pairs.panels(Z_std[,1:4], 
-             method = "pearson", # correlation method
-             hist.col = "#00AFBB",
-             density = TRUE,  # show density plots
-             ellipses = TRUE # show correlation ellipses
-)
+# What are the clusters?
+clust1$center  # not super helpful
+clust1$center[1,]*sigma + mu
+clust1$center[2,]*sigma + mu
 
-# Show the model fit
-par(mfrow=c(2,2))
-plot(fitted(pcr1), y_wine_quality, main="Wine Quality and PC 1")
-plot(seq(1,11,by=1), pc_wine$rotation[,1])
-plot(seq(1,11,by=1), pc_wine$rotation[,2])
-plot(seq(1,11,by=1), pc_wine$rotation[,3])
+# Which wines are in which clusters?
+which(clust1$cluster == 1)
+which(clust1$cluster == 2)
+
+clust2$center[1,]*sigma + mu
+clust2$center[2,]*sigma + mu
+
+# Which cars are in which clusters?
+which(clust2$cluster == 1)
+which(clust2$cluster == 2)
+
+# Compare versus within-cluster average distances from the first run
+clust1$withinss
+clust2$withinss
+sum(clust1$withinss)
+sum(clust2$withinss)
+clust1$tot.withinss
+clust2$tot.withinss
+clust1$betweenss
+clust2$betweenss
+
+# A few plots with cluster membership shown
+# qplot is in the ggplot2 library
+qplot(fixed.acidity, volatile.acidity, data=wine, color=factor(clust2$cluster))
+qplot(citric.acid, residual.sugar, data=wine, color=factor(clust2$cluster))
+qplot(quality, color, data=wine, color=factor(clust2$cluster))
+       
