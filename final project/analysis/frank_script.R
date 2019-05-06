7c26e438547d34807d42e8ae53981420070e8307
Frank-FYC <Frank.FYC@gmail.com>
2019-05-06 18:08:30 -0500

    additional work

--- final project/analysis/frank_script.R
+++ final project/analysis/frank_script.R
@@ -4,3 +4,8 @@ library(foreign)
 df <- read.dta("../data/nlsy_deming.dta")
 df2 <- read.dta("../data/data_Deming_2008_0217.dta")
 
+df_headstart <- subset(df,head_start==1)
+df_nonheadstart <- subset(df,head_start==0)
+
+df_headstart <- subset(df,head_start==1)
+df_nonheadstart <- subset(df,head_start==0)
