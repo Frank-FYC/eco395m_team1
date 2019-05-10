1d40040b77dbaa9e599d1ff0a066e9b3b2ba0891
Frank-FYC <Frank.FYC@gmail.com>
2019-05-09 19:22:01 -0500

    inprogress

--- final project/analysis/frank_script.R
+++ final project/analysis/frank_script.R
@@ -1,10 +1,22 @@
 library(tidyverse)
 library(foreign)
+library(dummy)
+library(caret)
+library(fastDummies)
 
-df <- read.dta("../data/nlsy_deming.dta")
-df2 <- read.dta("../data/data_Deming_2008_0217.dta")
+df <- read.dta("../data/data_Deming_2008_0217.dta")
 
-df_headstart <- subset(df,head_start==1)
-df_nonheadstart <- subset(df,head_start==0)
 
-birthweight <- subset(df2,BirthWeight=0:100)
+Age_1stHS88 
+Age_1stHS90 
+Age_1stHS92 
+Age_1stHS94 
+Age_1stHS96 
+Age_1stHS98 
+Age_1stHS100 
+Age_1stHS102 
+Age_1stHS104
+
+varlist <- c("BirthWeight")
+
+mental.disability <- ifelse(rowSums(select(df,Retard86:Retard100),na.rm = TRUE)==0,0,1)
