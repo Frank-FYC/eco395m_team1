f65e6a898f0fd593783e43a8ac643e7ffd55eb98
Frank-FYC <Frank.FYC@gmail.com>
2019-05-12 19:39:05 -0500

    done!

--- final project/analysis/final dataset processing.R
+++ final project/analysis/final dataset processing.R
@@ -83,83 +83,72 @@ df$childage2000 <- calc_age(2000,df$DOB_Yr_Child)
 df$childage2002 <- calc_age(2002,df$DOB_Yr_Child)
 df$childage2004 <- calc_age(2004,df$DOB_Yr_Child)
 
-compscorefunc <- function(year,column1,column2,age1){
-  df_comscorefunc <- subset(df,calc_age(year,df$DOB_Yr_Child)==age1)
-  df_comscorefunc_result <- cbind(df_comscorefunc[,column1],df_comscorefunc[,column2])
+compscorefunc <- function(age1){
+  df_comscorefunc1986 <- subset(df,calc_age(1986,df$DOB_Yr_Child)==age1)
+  df_comscorefunc1986_result <- cbind(df_comscorefunc1986[,"ChildID"],df_comscorefunc1986[,"Test_Pct86"])
+  df_comscorefunc1988 <- subset(df,calc_age(1988,df$DOB_Yr_Child)==age1)
+  df_comscorefunc1988_result <- cbind(df_comscorefunc1988[,"ChildID"],df_comscorefunc1988[,"Test_Pct88"])
+  df_comscorefunc1990 <- subset(df,calc_age(1990,df$DOB_Yr_Child)==age1)
+  df_comscorefunc1990_result <- cbind(df_comscorefunc1990[,"ChildID"],df_comscorefunc1990[,"Test_Pct90"])
+  df_comscorefunc1992 <- subset(df,calc_age(1992,df$DOB_Yr_Child)==age1)
+  df_comscorefunc1992_result <- cbind(df_comscorefunc1992[,"ChildID"],df_comscorefunc1992[,"Test_Pct92"])
+  df_comscorefunc1994 <- subset(df,calc_age(1994,df$DOB_Yr_Child)==age1)
+  df_comscorefunc1994_result <- cbind(df_comscorefunc1994[,"ChildID"],df_comscorefunc1994[,"Test_Pct94"])
+  df_comscorefunc1996 <- subset(df,calc_age(1996,df$DOB_Yr_Child)==age1)
+  df_comscorefunc1996_result <- cbind(df_comscorefunc1996[,"ChildID"],df_comscorefunc1996[,"Test_Pct96"])
+  df_comscorefunc1998 <- subset(df,calc_age(1998,df$DOB_Yr_Child)==age1)
+  df_comscorefunc1998_result <- cbind(df_comscorefunc1998[,"ChildID"],df_comscorefunc1998[,"Test_Pct98"])
+  df_comscorefunc2000 <- subset(df,calc_age(2000,df$DOB_Yr_Child)==age1)
+  df_comscorefunc2000_result <- cbind(df_comscorefunc2000[,"ChildID"],df_comscorefunc2000[,"Test_Pct100"])
+  df_comscorefunc2002 <- subset(df,calc_age(2002,df$DOB_Yr_Child)==age1)
+  df_comscorefunc2002_result <- cbind(df_comscorefunc2002[,"ChildID"],df_comscorefunc2002[,"Test_Pct102"])
+  df_comscorefunc2004 <- subset(df,calc_age(2004,df$DOB_Yr_Child)==age1)
+  df_comscorefunc2004_result <- cbind(df_comscorefunc2004[,"ChildID"],df_comscorefunc2004[,"Test_Pct104"])
+  rbind(df_comscorefunc1986_result,df_comscorefunc1988_result,df_comscorefunc1990_result,df_comscorefunc1992_result,
+        df_comscorefunc1994_result,df_comscorefunc1996_result,df_comscorefunc1998_result,df_comscorefunc2000_result,
+        df_comscorefunc2002_result,df_comscorefunc2004_result)
 }
 
-compscoreage6 <- rbind(
-  compscorefunc(1986,"ChildID","Test_Pct86","6"),
-  compscorefunc(1988,"ChildID","Test_Pct88","6"),
-  compscorefunc(1990,"ChildID","Test_Pct90","6"),
-  compscorefunc(1992,"ChildID","Test_Pct92","6"),
-  compscorefunc(1994,"ChildID","Test_Pct94","6"),
-  compscorefunc(1996,"ChildID","Test_Pct96","6"),
-  compscorefunc(1998,"ChildID","Test_Pct98","6"),
-  compscorefunc(2002,"ChildID","Test_Pct100","6"),
-  compscorefunc(2002,"ChildID","Test_Pct102","6"),
-  compscorefunc(2004,"ChildID","Test_Pct104","6")
-) %>% as.data.frame() %>% unique()
+compscoreage6 <- compscorefunc("6")
+colnames(compscoreage6) <- c("ChildID","compscoreage6")
+compscoreage7 <- compscorefunc("7")
+colnames(compscoreage7) <- c("ChildID","compscoreage7")
+compscoreage7 <- compscorefunc("7")
+colnames(compscoreage7) <- c("ChildID","compscoreage8")
+compscoreage9 <- compscorefunc("9")
+colnames(compscoreage9) <- c("ChildID","compscoreage9")
+compscoreage10 <- compscorefunc("10")
+colnames(compscoreage10) <- c("ChildID","compscoreage10")
+compscoreage11 <- compscorefunc("11")
+colnames(compscoreage11) <- c("ChildID","compscoreage11")
+compscoreage12 <- compscorefunc("12")
+colnames(compscoreage12) <- c("ChildID","compscoreage12")
+compscoreage13 <- compscorefunc("13")
+colnames(compscoreage13) <- c("ChildID","compscoreage13")
+compscoreage14 <- compscorefunc("14")
+colnames(compscoreage14) <- c("ChildID","compscoreage14")
+compscoreage15 <- compscorefunc("15")
+colnames(compscoreage15) <- c("ChildID","compscoreage15")
+compscoreage16 <- compscorefunc("16")
+colnames(compscoreage16) <- c("ChildID","compscoreage16")
+compscoreage17 <- compscorefunc("17")
+colnames(compscoreage17) <- c("ChildID","compscoreage17")
+compscoreage18 <- compscorefunc("18")
+colnames(compscoreage18) <- c("ChildID","compscoreage18")
 
-names(compscoreage6) <- c("ChildID","compscoreage6")
-
-compscoreage11 <- rbind(
-  compscorefunc(1986,"ChildID","Test_Pct86","11"),
-  compscorefunc(1988,"ChildID","Test_Pct88","11"),
-  compscorefunc(1990,"ChildID","Test_Pct90","11"),
-  compscorefunc(1992,"ChildID","Test_Pct92","11"),
-  compscorefunc(1994,"ChildID","Test_Pct94","11"),
-  compscorefunc(1996,"ChildID","Test_Pct96","11"),
-  compscorefunc(1998,"ChildID","Test_Pct98","11"),
-  compscorefunc(2002,"ChildID","Test_Pct100","11"),
-  compscorefunc(2002,"ChildID","Test_Pct102","11"),
-  compscorefunc(2004,"ChildID","Test_Pct104","11")
-) %>% as.data.frame() %>% unique()
-
-names(compscoreage11) <- c("ChildID","compscoreage11")
-
-compscoreage14 <- rbind(
-  compscorefunc(1986,"ChildID","Test_Pct86","14"),
-  compscorefunc(1988,"ChildID","Test_Pct88","14"),
-  compscorefunc(1990,"ChildID","Test_Pct90","14"),
-  compscorefunc(1992,"ChildID","Test_Pct92","14"),
-  compscorefunc(1994,"ChildID","Test_Pct94","14"),
-  compscorefunc(1996,"ChildID","Test_Pct96","14"),
-  compscorefunc(1998,"ChildID","Test_Pct98","14"),
-  compscorefunc(2002,"ChildID","Test_Pct100","14"),
-  compscorefunc(2002,"ChildID","Test_Pct102","14"),
-  compscorefunc(2004,"ChildID","Test_Pct104","14")
-) %>% as.data.frame() %>% unique()
-
-names(compscoreage14) <- c("ChildID","compscoreage14")
-
-compscoreage18 <- rbind(
-  compscorefunc(1986,"ChildID","Test_Pct86","18"),
-  compscorefunc(1988,"ChildID","Test_Pct88","18"),
-  compscorefunc(1990,"ChildID","Test_Pct90","18"),
-  compscorefunc(1992,"ChildID","Test_Pct92","18"),
-  compscorefunc(1994,"ChildID","Test_Pct94","18"),
-  compscorefunc(1996,"ChildID","Test_Pct96","18"),
-  compscorefunc(1998,"ChildID","Test_Pct98","18"),
-  compscorefunc(2002,"ChildID","Test_Pct100","18"),
-  compscorefunc(2002,"ChildID","Test_Pct102","18"),
-  compscorefunc(2004,"ChildID","Test_Pct104","18")
-) %>% as.data.frame() %>% unique()
-
-names(compscoreage18) <- c("ChildID","compscoreage18")
-
-test <- merge(compscoreage6,unique(compscoreage11),by="ChildID")
-
-df2 <- merge(compscoreage6,compscoreage11,by="ChildID",all=TRUE)
-df2 <- merge(df2,compscoreage14,by="ChildID",all=TRUE)
-df2 <- merge(df2,compscoreage18,by="ChildID",all=TRUE)
-df2 <- unique(df2)
-
-# david's contribution
-
-#sibdiff <- select((df, ifelse(df$MotherID ))) # Not working yet
-
-# tessie's contribution
+finaldataset <- merge(finaldataset,compscoreage6,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage7,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage8,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage9,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage10,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage11,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage12,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage13,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage14,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage15,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage16,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage17,by="ChildID",all=TRUE)
+finaldataset <- merge(finaldataset,compscoreage18,by="ChildID",all=TRUE)
 
 # selecting the final variables for the final dataset
 
